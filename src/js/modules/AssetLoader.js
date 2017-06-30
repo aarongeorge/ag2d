72c4baf12a8a6441ba5ef4f60317ac6341f010ae
Aaron George <aaron.dean.george@gmail.com>
2017-06-30 17:37:52 +1000

    ADDED: Video loader to asset loader

--- src/js/modules/AssetLoader.js
+++ src/js/modules/AssetLoader.js
@@ -5,8 +5,7 @@
  */
 
 // Constructor: AssetLoader
-var AssetLoader = function () {
-    'use strict';
+const AssetLoader = function () {
 
     this.assets = {};
     this.assetsToLoad = [];
@@ -15,7 +14,6 @@ var AssetLoader = function () {
 
 // Method: addAssets
 AssetLoader.prototype.addAssets = function (assets) {
-    'use strict';
 
     // `assets` is not an array
     if (Object.prototype.toString.call(assets) !== '[object Array]') {
@@ -28,59 +26,89 @@ AssetLoader.prototype.addAssets = function (assets) {
     else {
 
         // Iterate over `assets`
-        for (var i = 0; i < assets.length; i++) {
+        for (let i = 0; i < assets.length; i++) {
 
             // Asset doesn't have a type
-            if (!assets[i].type) {
-
-                // Throw error
-                throw new Error('Asset does not have a type', assets[i]);
-            }
-
-            // Asset does have a type
-            else {
+            if (assets[i].type) {
 
                 // Switch over `type`
                 switch (assets[i].type) {
 
                     // Image
-                    case 'image': {
+                case 'image': {
 
-                        // Asset doesn't have a path
-                        if (!assets[i].path) {
-
-                            // Throw error
-                            throw new Error('Asset does not have a path', assets[i]);
-                        }
-
-                        // Asset does have a path
-                        else {
-
-                            // Asset doesn't have a name
-                            if (!assets[i].name) {
-
-                                // Throw error
-                                throw new Error('Asset does not have a name', assets[i]);
-                            }
-
-                            // Asset has everything we need
-                            else {
-
-                                // Add asset to `assetsToLoad`
-                                this.assetsToLoad.push(assets[i]);
-                            }
-                        }
-
-                        break;
-                    }
-
-                    // Invalid type
-                    default: {
+                    // Asset doesn't have a path
+                    if (!assets[i].path) {
 
                         // Throw error
-                        throw new Error('Asset is not a valid type');
+                        throw new Error('Asset does not have a path', assets[i]);
                     }
+
+                    // Asset does have a path
+                    else {
+
+                        // Asset doesn't have a name
+                        if (!assets[i].name) {
+
+                            // Throw error
+                            throw new Error('Asset does not have a name', assets[i]);
+                        }
+
+                        // Asset has everything we need
+                        else {
+
+                            // Add asset to `assetsToLoad`
+                            this.assetsToLoad.push(assets[i]);
+                        }
+                    }
+
+                    break;
                 }
+
+                case 'video': {
+
+                    // Asset doesn't have a path
+                    if (!assets[i].path) {
+
+                        // Throw error
+                        throw new Error('Asset does not have a path', assets[i]);
+                    }
+
+                    // Asset does have a path
+                    else {
+
+                        // Asset doesn't have a name
+                        if (!assets[i].name) {
+
+                            // Throw error
+                            throw new Error('Asset does not have a name', assets[i]);
+                        }
+
+                        // Asset has everything we need
+                        else {
+
+                            // Add asset to `assetsToLoad`
+                            this.assetsToLoad.push(assets[i]);
+                        }
+                    }
+
+                    break;
+                }
+
+                // Invalid type
+                default: {
+
+                            // Throw error
+                    throw new Error('Asset is not a valid type');
+                }
+                }
+            }
+
+            // Asset doesn have a type
+            else {
+
+                // Throw error
+                throw new Error('Asset does not have a type', assets[i]);
             }
         }
     }
@@ -88,10 +116,10 @@ AssetLoader.prototype.addAssets = function (assets) {
 
 // Method: loadAssets
 AssetLoader.prototype.loadAssets = function (callback) {
-    'use strict';
+
 
     // Store reference to `this`
-    var _this = this;
+    const _this = this;
 
     // Make sure there are still assets to load
     if (this.assetsToLoad.length > 0) {
@@ -100,7 +128,7 @@ AssetLoader.prototype.loadAssets = function (callback) {
         this.assetsLoaded = false;
 
         // Load the asset
-        this.loadAsset(this.assetsToLoad[0], function () {
+        this.loadAsset(this.assetsToLoad[0], () => {
 
             // Add the asset to `assets`
             _this.assets[_this.assetsToLoad[0].name] = _this.assetsToLoad[0];
@@ -126,36 +154,44 @@ AssetLoader.prototype.loadAssets = function (callback) {
 
 // Method: loadAsset
 AssetLoader.prototype.loadAsset = function (asset, callback) {
-    'use strict';
+
 
     // Switch on `type`
     switch (asset.type) {
 
         // Image
-        case 'image': {
+    case 'image': {
 
-            // Call `loadImage`
-            this.loadImage(asset, callback);
+        // Call `loadImage`
+        this.loadImage(asset, callback);
 
-            break;
-        }
+        break;
+    }
 
-        // Default
-        default: {
-            throw new Error('Asset has no type');
-        }
+    case 'video': {
+
+        // Call `loadVideo`
+        this.loadVideo(asset, callback);
+
+        break;
+    }
+
+    // Default
+    default: {
+        throw new Error('Asset has no type');
+    }
     }
 };
 
 // Method: loadImage
 AssetLoader.prototype.loadImage = function (asset, callback) {
-    'use strict';
+
 
     // Create new image
-    var img = new Image();
+    const img = new Image();
 
     // On load of image
-    img.addEventListener('load', function () {
+    img.addEventListener('load', () => {
 
         // Add element
         asset.element = this;
@@ -165,7 +201,7 @@ AssetLoader.prototype.loadImage = function (asset, callback) {
     });
 
     // On error of image
-    img.addEventListener('error', function (e) {
+    img.addEventListener('error', (e) => {
 
         // Throw error
         throw new Error(e);
@@ -176,6 +212,84 @@ AssetLoader.prototype.loadImage = function (asset, callback) {
 
     // Set `img.src`
     img.src = asset.path;
+};
+
+// Method: createVideo
+AssetLoader.prototype.loadVideo = function (asset, callback) {
+
+    // Create new video
+    const video = document.createElement('video');
+
+    // Create source
+    const sourceEl = document.createElement('source');
+
+    asset.element = video;
+    asset.volume = typeof asset.volume === 'undefined' ? 1 : asset.volume;
+    asset.loaded = 0;
+
+    video.autoplay = typeof asset.autoplay === 'undefined' ? true : asset.autoplay;
+    video.loop = typeof asset.loop === 'undefined' ? true : asset.loop;
+    video.muted = typeof asset.muted === 'undefined' ? false : asset.muted;
+
+    asset.play = function (currentTime) {
+        if (typeof currentTime !== 'undefined') {
+            video.currentTime = currentTime;
+        }
+        video.play();
+        if (asset.onStart) {
+            asset.onStart();
+        }
+    };
+
+    asset.pause = function () {
+        video.pause();
+    };
+
+    asset.resume = function () {
+        video.play();
+    };
+
+    asset.stop = function () {
+        video.pause();
+        video.currentTime = 0;
+    };
+
+    video.addEventListener('canplaythrough', () => {
+        if (!asset.ready) {
+            asset.ready = true;
+            return callback();
+        }
+    });
+
+    video.addEventListener('loadeddata', () => {
+
+        // First frame loaded
+        asset.width = this.videoWidth;
+        asset.height = this.videoHeight;
+        asset.duration = this.duration;
+    });
+
+    video.addEventListener('ended', () => {
+        if (asset.onEnd) {
+            asset.onEnd();
+        }
+    });
+
+    // On error of video
+    video.addEventListener('error', (e) => {
+
+        // Throw error
+        throw new Error(e);
+    });
+
+    // Add type
+    sourceEl.type = `video/${asset.path.match(/\.[0-9a-z]+$/i)[0].substring(1)}`;
+
+    // Add source
+    sourceEl.src = asset.path;
+
+    // Append `sourceEl` to `video`
+    video.appendChild(sourceEl);
 };
 
 // Export `AssetLoader`
