ce37ed986420d5f6db6915fdae770384fa123e07
Aaron George <aaron.dean.george@gmail.com>
2017-12-10 15:38:54 +1100

    FIXED: Animation being completely busted when using anything other than the first few frames

--- src/js/modules/Animation.js
+++ src/js/modules/Animation.js
@@ -32,161 +32,97 @@ class Animation {
 
         // Set defaults
         this.animate = false;
-        this.currentFrame = 0;
+        this.currentFrameIndex = this.getStartFrameIndex();
         this.interval = 1000 / options.fps;
         this.lastDraw = currentTime;
         this.lastUpdate = currentTime;
     }
 
-    // Method: update
-    update (deltaTime) {
-
-        if (!this.animate) {
-            return;
-        }
-
-        // Calculate delta time
-        const drawDeltaTime = this.lastUpdate - this.lastDraw;
-
-        // If `deltaTime` is higher than `interval`
-        if (drawDeltaTime > this.interval) {
-
-            // Call `nextFrame`
-            this.nextFrame();
-
-            // Update `lastDraw`
-            this.lastDraw = this.lastUpdate - drawDeltaTime % this.interval;
-        }
-
-        // Update `lastUpdate`
-        this.lastUpdate += deltaTime;
+    // Method: getEndFrameIndex
+    getEndFrameIndex () {
+        return this.reverse ? 0 : this.frames.length - 1;
     }
 
-    // Method: getFrame
-    getFrame () {
-
-        // Return `currentFrame`
-        return this.currentFrame;
-    }
-
-    // Method: nextFrame
-    nextFrame () {
-
-        // `currentFrame` is `endFrame`
-        if (this.frames.indexOf(this.currentFrame) === this.getEndFrame()) {
-
-            // If `loop` is set
+    // Method: nextFrameIndex
+    getNextFrameIndex () {
+        if (this.currentFrameIndex === this.getEndFrameIndex()) {
             if (this.loop) {
-
-                // `loopType` is `wrap`
                 if (this.loopType === 'wrap') {
-
-                    // Flip `reverse`
                     this.reverse = !this.reverse;
                 }
 
-                // Call `restart`
                 this.restart();
             }
 
-            // `loop` isn't set
             else {
-
-                // Call `stop`
                 this.stop();
             }
         }
 
-        // `currentFrame` is not `endFrame`
         else {
-
-            // Set `currentFrame` based on `reverse`
-            this.currentFrame = this.reverse ? this.currentFrame - 1 : this.currentFrame + 1;
+            this.currentFrameIndex = this.reverse ? this.currentFrameIndex - 1 : this.currentFrameIndex + 1;
         }
     }
 
-    // Method: start
-    start () {
-
-        // Set `animate` to `true`
-        this.animate = true;
-
-        // Call `startCb`
-        this.startCb();
+    // Method: getStartFrameIndex
+    getStartFrameIndex () {
+        return this.reverse ? this.frames.length - 1 : 0;
     }
 
     // Method: pause
     pause () {
-
-        // Set `animate` to `false`
         this.animate = false;
-
-        // Call `pauseCb`
         this.pauseCb();
-    }
-
-    // Method: stop
-    stop () {
-
-        // Set `currentFrame` to start frame
-        this.currentFrame = this.getStartFrame();
-
-        // Set `animate` to `false`
-        this.animate = false;
-
-        // Call `stopCb`
-        this.stopCb();
-    }
-
-    // Method: restart
-    restart () {
-
-        // Set `currentFrame` to start frame
-        this.currentFrame = this.getStartFrame();
-
-        // Set `animate` to `true`
-        this.animate = true;
-
-        // Call `restartCb`
-        this.restartCb();
-    }
-
-    // Method: reset
-    reset () {
-
-        // Set `currentFrame` to start frame
-        this.currentFrame = this.getStartFrame();
-
-        // Set `animate` to `false`
-        this.animate = false;
-
-        // Call `resetCb`
-        this.resetCb();
-    }
-
-    // Method: getStartFrame
-    getStartFrame () {
-
-        // If `reverse` return the last item in `frames` else return the first item in `frames`
-        return this.reverse ? this.frames[this.frames.length - 1] : this.frames[0];
-    }
-
-    // Method: getEndFrame
-    getEndFrame () {
-
-        // If `reverse` return the first item in `frames` else return the last item in `frames`
-        return this.reverse ? this.frames[0] : this.frames[this.frames.length - 1];
     }
 
     // Method: render
     render (context, x, y, width = this.spriteSheet.frameWidth, height = this.spriteSheet.frameHeight, cropWidth = this.spriteSheet.frameWidth, cropHeight = this.spriteSheet.frameHeight) {
+        const row = Math.floor(this.frames[this.currentFrameIndex] / this.spriteSheet.columns);
+        const col = Math.floor(this.frames[this.currentFrameIndex] % this.spriteSheet.columns);
 
-        // Get `row` and `col` for `currentFrame`
-        const row = Math.floor(this.frames[this.currentFrame] / this.spriteSheet.columns);
-        const col = Math.floor(this.frames[this.currentFrame] % this.spriteSheet.columns);
+        context.drawImage(this.spriteSheet.image, col * this.spriteSheet.frameWidth, row * this.spriteSheet.frameHeight, cropWidth, cropHeight, x, y, width, height);
+    }
 
-        // Draw `spriteSheet`
-        context.drawImage(this.spriteSheet.image, col * this.spriteSheet.frameWidth, row * this.spriteSheet.frameHeight, width, height, x, y, cropWidth, cropHeight);
+    // Method: reset
+    reset () {
+        this.currentFrameIndex = this.getStartFrameIndex();
+        this.animate = false;
+        this.resetCb();
+    }
+
+    // Method: restart
+    restart () {
+        this.currentFrameIndex = this.getStartFrameIndex();
+        this.animate = true;
+        this.restartCb();
+    }
+
+    // Method: start
+    start () {
+        this.animate = true;
+        this.startCb();
+    }
+
+    // Method: stop
+    stop () {
+        this.currentFrameIndex = this.getStartFrameIndex();
+        this.animate = false;
+        this.stopCb();
+    }
+
+    // Method: update
+    update (deltaTime) {
+        if (!this.animate) {
+            return;
+        }
+
+        const drawDeltaTime = this.lastUpdate - this.lastDraw;
+        if (drawDeltaTime > this.interval) {
+            this.getNextFrameIndex();
+            this.lastDraw = this.lastUpdate - drawDeltaTime % this.interval;
+        }
+
+        this.lastUpdate += deltaTime;
     }
 }
 
