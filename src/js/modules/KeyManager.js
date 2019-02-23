ca7e4ca68c116b9d131813abb24bdfdd593c4d4d
Aaron George <aaron.dean.george@gmail.com>
2019-02-24 01:49:58 +1100

    UPDATED: eslint config
    UPDATED: Key Manager now uses Maps instead of arrays, and only stores a boolean value to say if a key is pressed or not

--- src/js/modules/KeyManager.js
+++ src/js/modules/KeyManager.js
@@ -4,36 +4,96 @@
  * @desc A key manager
  */
 
+// Dependencies
+import EventHandler from './EventHandler';
+
 // Constructor: AnimationManager
 class KeyManager {
 
     // Constructor
     constructor () {
-        this.keysDown = {};
+
+        // Create `eventHandler`
+        this.eventHandler = new EventHandler();
+
+        // Map to store each key we are mapping to
+        this.keyMap = new Map();
+
+        // Map to store the state of each key in `keyMap`
+        this.keyStates = new Map();
+
+        // An array of keyboard event names to listen to
+        this.keyboardEventNames = [
+            'keydown',
+            'keyup'
+        ];
     }
 
-    // Method: keyUp
-    keyUp (e) {
-
-        // Set the key to false
-        this.keysDown[e.keyCode] = false;
+    // Method: addMapping
+    addMapping (keyCode, callback) {
+        this.keyMap.set(keyCode, callback);
     }
 
-    // Method: keyDown
-    keyDown (e) {
+    // Method: disable
+    disable (target = window) {
 
-        if (!this.isDown(e.keyCode)) {
+        // Iterate over `keyboardEventNames`
+        this.keyboardEventNames.forEach(() => {
 
-            // Set the value to the current time
-            this.keysDown[e.keyCode] = window.performance.now();
+            // Remove `eventName` handlers
+            this.eventHandler.removeEvent('handleEvent');
+        });
+    }
+
+    // Method: enable
+    enable (target = window) {
+
+        // Iterate over `keyboardEventNames`
+        this.keyboardEventNames.forEach(eventName => {
+
+            // Create event handler for `eventName`
+            this.eventHandler.addEvent({
+                'element': target,
+                'function': this.handleEvent.bind(this),
+                'name': `handleEvent:${eventName}`,
+                'type': eventName
+            });
+        });
+    }
+
+    // Method: handleEvent
+    handleEvent (event) {
+
+        // `keyMap` has a binding
+        if (this.keyMap.has(event.code) === true) {
+
+            // Set `keyState` to `1` if key is pressed, or `0` if it is not
+            const keyState = event.type === 'keydown' ? 1 : 0;
+
+            // `keyState`has changed
+            if (this.keyStates.get(event.code) !== keyState) {
+
+                // Update `keyState`
+                this.keyStates.set(event.code, keyState);
+
+                // Call the callback for the current key
+                this.keyMap.get(event.code)(keyState);
+            }
         }
     }
 
-    // Method: isDown
-    isDown (key) {
+    // Method: removeMapping
+    removeMapping (keyCode) {
 
-        // Return if the key is down
-        return Boolean(this.keysDown[key]);
+        // Remove `keyCode` from the `keyStates` map
+        this.keyMap.delete(keyCode);
+
+        // `keyStates` map has `keyCode` in it
+        if (this.keyStates.has(keyCode)) {
+
+            // Remove `keyCode` from the `keyStates` map
+            this.keyStates.delete(keyCode);
+        }
     }
 }
 
